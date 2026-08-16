9f76f0a90a7d5fb053ba4000fbaf153279bb1216
Keisuke Komura <kkeisuke@meijo-u.ac.jp>
2026-08-16 10:04:03 +0900

    deploy 2026-08-16T01:04:03.033Z

---
+++ sw.js
@@ -0,0 +1,49 @@
+// 自動生成 Service Worker (scripts/gen-sw.mjs)
+const CACHE = 'kanji-quest-6ca34ec85b'
+const ASSETS = [
+  "./",
+  "./assets/index-BUpeU6my.js",
+  "./assets/index-KfWDWaIY.css",
+  "./icons/apple-touch-icon.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./robots.txt"
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()))
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request
+  if (req.method !== 'GET') return
+  const url = new URL(req.url)
+  if (url.origin !== self.location.origin) return
+  if (req.mode === 'navigate') {
+    e.respondWith(fetch(req).catch(() => caches.match('./index.html')))
+    return
+  }
+  e.respondWith(
+    caches.match(req, { ignoreSearch: true }).then(
+      (hit) =>
+        hit ||
+        fetch(req).then((res) => {
+          if (res.ok) {
+            const clone = res.clone()
+            caches.open(CACHE).then((c) => c.put(req, clone))
+          }
+          return res
+        })
+    )
+  )
+})
